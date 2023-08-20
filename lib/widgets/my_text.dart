88ef553742c25a97615046194247309f978bbf35
Priyanshu078 <priyanshupaliwal078@gmail.com>
2023-08-20 13:53:24 +0530

    creating a dope tictactoe app

---
+++ lib/widgets/my_text.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'goo';
+
+class MyText extends StatelessWidget {
+  const MyText({super.key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text, style: GoogleFonts.,);
+  }
+}
